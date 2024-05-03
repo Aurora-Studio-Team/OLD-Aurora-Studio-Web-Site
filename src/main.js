d982fecabae9a6bed9b42468e723b9ba3cd11321
LiteCat <littttt87@hotmail.com>
2024-05-03 19:05:13 +0800

    修复main.ts

---
+++ src/main.js
@@ -0,0 +1,18 @@
+import { createApp } from 'vue'
+import './style.css'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+import App from './App.vue'
+import router from './router';
+
+
+createApp(App).mount('#app')
+const app = createApp(App)
+app.use(router)
+app.use(ElementPlus, { size: 'small', zIndex: 3000 })
+app.use(ElementPlus)
+app.mount('#app')
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+    app.component(key, component)
+}
