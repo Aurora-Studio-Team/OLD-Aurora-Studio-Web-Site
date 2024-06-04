b2e1dec82499976eceb76b6a119c2018f5039aea
THZ <thz15916108729@163.com>
2024-06-04 17:26:05 +0800

    1.5

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -5,30 +5,60 @@
 <template>
     <div>
         <div class="Title-Card">
-            <h1>Aurora Studio</h1>
-            <h2>Unleash your creativity, <br/>software shapes the future.</h2>
+            <h1>UNLEASH YOUR <br/>CREATIVITY, <br/>SOFTWARE SHAPES <br/>THE FUTURE.</h1>
             <br/>
             <div>
                 <el-button size="large" type="primary" round><a href="./Apps">了解产品</a></el-button>
                 <el-button size="large" disabled round>获取Hub</el-button>
             </div>
         </div>
-        <div class="Show-Card">
+        <div class="Title-Show-Card">
             <img alt="Big-Logo" class="Show-Pictures" src="\img\Show-Pictures\Big-Logo.png"/>
         </div>
     </div>
     <div class="Show-List">
+        <br/>
+        <div class="Show-List-TextBox">
+            
+        </div>
         <div class="Show-List-Card">
             <svg t="1717409720863" class="Show-List-Card-Icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="1496" width="200" height="200"><path d="M512 85.333333c72.533333 0 139.808 8.362667 200.202667 22.325334 21.258667 4.906667 40.192 10.208 56.586666 15.52 9.301333 3.018667 16.106667 5.472 20.245334 7.114666C831.733333 144.906667 864 192.106667 864 238.624v385.034667c0 43.402667-25.130667 95.52-58.837333 121.109333l-227.626667 172.810667c-36.992 28.085333-94.005333 28.149333-131.072 0l-227.626667-172.810667C185.28 719.285333 160 666.912 160 623.658667V238.624c0-46.826667 32.170667-94.218667 75.157333-108.405333 4.16-1.642667 10.901333-4.074667 20.053334-7.04a684.437333 684.437333 0 0 1 56.586666-15.52C372.192 93.685333 439.466667 85.333333 512 85.333333z m0 64c-67.456 0-129.930667 7.765333-185.781333 20.682667-19.424 4.48-36.586667 9.28-51.274667 14.048-5.056 1.632-9.418667 3.146667-13.056 4.48-2.058667 0.746667-3.349333 1.248-3.84 1.450667a32 32 0 0 1-2.485333 0.885333c-16.074667 5.034667-31.562667 27.626667-31.562667 47.744v385.034667c0 23.498667 16.128 56.917333 33.536 70.133333l227.626667 172.810667c14.176 10.773333 39.530667 10.741333 53.674666 0l227.626667-172.810667C784 680.469333 800 647.306667 800 623.658667V238.624c0-19.84-15.626667-42.506667-32-47.882667l-2.048-0.746666a143.829333 143.829333 0 0 0-3.84-1.450667c-3.637333-1.333333-8-2.848-13.056-4.48a620.778667 620.778667 0 0 0-51.274667-14.048C641.930667 157.098667 579.456 149.333333 512 149.333333z m137.354667 190.72a32 32 0 0 1 45.290666 45.226667l-213.013333 213.333333a32 32 0 0 1-45.226667 0.064l-106.986666-106.549333a32 32 0 1 1 45.162666-45.344l84.341334 83.989333z" fill="#000000" p-id="1497"></path></svg>
-            <h3>安全</h3>
+            <h1>安全</h1>
+            <p>我们的软件无毒无害，并且不会收集你的隐私资料，你可以放心使用！</p>
         </div>
         <div class="Show-List-Card">
             <svg t="1717409430306" class="Show-List-Card-Icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="1630" width="200" height="200"><path d="M395.765333 586.570667h-171.733333c-22.421333 0-37.888-22.442667-29.909333-43.381334L364.768 95.274667A32 32 0 0 1 394.666667 74.666667h287.957333c22.72 0 38.208 23.018667 29.632 44.064l-99.36 243.882666h187.050667c27.509333 0 42.186667 32.426667 24.042666 53.098667l-458.602666 522.56c-22.293333 25.408-63.626667 3.392-54.976-29.28l85.354666-322.421333zM416.714667 138.666667L270.453333 522.581333h166.869334a32 32 0 0 1 30.933333 40.181334l-61.130667 230.954666 322.176-367.114666H565.312c-22.72 0-38.208-23.018667-29.632-44.064l99.36-243.882667H416.714667z" fill="#000000" p-id="1631"></path></svg>        
-            <h3>快速</h3>
+            <h1>快速</h1>
+            <p>我们的软件启动迅速，不需要等待太久时间！</p>
         </div>
         <div class="Show-List-Card">
             <svg t="1717409791850" class="Show-List-Card-Icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="1469" width="200" height="200"><path d="M171.712 571.648l0.352 0.32 287.904 252.8a64 64 0 0 0 82.912 1.344l296.832-244.544a215.584 215.584 0 1 0-301.824-300.576L512 316.672l-25.888-35.616a215.584 215.584 0 1 0-314.4 290.624zM32 407.584a279.584 279.584 0 0 1 480-194.944 279.584 279.584 0 0 1 480 194.944 278.144 278.144 0 0 1-113.024 224.512l-295.36 243.392a128 128 0 0 1-165.888-2.592L129.984 620.16A278.976 278.976 0 0 1 32 407.584z" fill="#000000" p-id="1470"></path></svg>
-            <h3>美观</h3>
+            <h1>美观</h1>
+            <p>我们的软件基本采用 Fluent 主题，简结美观！</p>
+        </div>
+        <br/>
+    </div>
+    <div class="Show-Apps">
+        <div>
+            <div class="Show-Apps-Card">
+                <h4>Aurora Star Launcher</h4>
+                <h1>快速启动和管理您的 <br/>Minecraft 游戏</h1>
+                <p>Aurora Star Launcher <br/>采用全新的.NET8.0,<br/>界面美观,启动迅速!</p>
+                <br/>
+                <el-button size="large" type="primary" round><a href="./Apps">了解产品</a></el-button>    
+            </div>
+            <div class="Show-Apps-Card-Picture">
+                <img src="/img/ASL.png" alt="ASL"/>
+            </div>
+        </div>
+    </div>
+    <div class="QandA">
+        <h2 style="text-align: center">常见问题回答</h2>
+        <br/>
+        <div class="QandA-Card">
+            <h3>Q：我现在的应用程序无法在我的计算机上运行?</h3>
+            <p>A:：你可以尝试安装 .Net8.0 运行库.</p>
+            <br/>
         </div>
     </div>
 </template>
@@ -42,9 +72,11 @@
         width: 70%;
         display:inline-block;
     }
-    .Show-Card{
+    .Title-Card h1{
+        font-size: 50px;
+    }
+    .Title-Show-Card{
         text-align: right;
-        margin-top: 3%;
         display:inline-block;
     }
     .Show-Pictures{
@@ -53,12 +85,13 @@
     }
     .Show-List{
         width: 100%;
-        background-color: #fdfdfdd7;
         border-radius: 10px;
         margin-top: 5%;
     }
     .Show-List-Card{
-        width: 33%;
+        margin-left: 5%;
+        margin-bottom: 5%;
+        width: 25%;
         text-align: center;
         display:inline-block;
     }
@@ -66,5 +99,61 @@
         margin-top: 5%;
         height: 55px;
         width: 55px;
+    }
+    .Show-Apps{
+        width: 100%;
+        margin-top: 5%;
+    }
+    .Show-Apps-Card{
+        text-align: left;
+        margin-left: 5%;
+        width: 60%;
+        border-radius: 10px;
+        display:inline-block;
+    }
+    .Show-Apps p{
+        text-align: center;
+    }
+    .End-Text{
+        text-align: center;
+    }
+    .QandA{
+        width: 100%;
+        margin-top: 5%;
+    }
+    .QandA-Card{
+        background-color: #fdfdfdd7;
+        border-radius: 10px;
+        box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+    }
+    .QandA-Card h3{
+        text-align: left;
+        margin-left: 5%;
+    }
+    .QandA-Card p{
+        text-align: left;
+        margin-left: 5%;
+    }
+    .Show-Apps-Card h4{
+        text-align: left;
+    }
+    .Show-Apps-Card h1{
+        text-align: left;
+        font-size: 35px;
+    }
+    .Show-Apps-Card p{
+        text-align: left;
+        font-size: 25px;
+    }
+    .Show-Apps-Card el-button{
+        text-align: left;
+    }
+    .Show-Apps-Card-Picture{
+        text-align: right;
+        border-radius: 10px;
+        margin-right: 5%;
+        width: 250px;
+        height: 250px;
+        display:inline-block;
     }
 </style>
